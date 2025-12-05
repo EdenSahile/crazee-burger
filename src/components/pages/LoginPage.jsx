9cf7197a7b714182c9a252b28fc53f65cf5c6bfd
Eden <edensahile28@gmail.com>
2025-12-05 20:17:58 +0100

    ✨ F01: LOGIN>Add title and form

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -3,11 +3,8 @@ import React, { useState } from 'react'
 export default function LoginPage() {
  const [inputValue, setInputValue] = useState("")
 
- const handleChange=(e)=>{
- setInputValue(e.target.value);
+ const handleChange=(e)=>setInputValue(e.target.value);
   
-
- }
 
  const handleSubmit=(e)=>{
   e.preventDefault()
@@ -21,7 +18,7 @@ export default function LoginPage() {
   return (
    
      <div>
-       <h1> Bienvenue chez nous ! </h1>
+       <h1> Bienvenue chez nous !</h1>
        <h2>Connectez-vous</h2>
        <form action="submit" onSubmit={handleSubmit}>
         <input type="text" value={inputValue} placeholder="Entrez votre prénom..."onChange={handleChange}  required/>
