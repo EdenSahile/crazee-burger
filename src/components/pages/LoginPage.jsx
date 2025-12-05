ebc1dad8725661523213c08151216465792f2cc6
Eden <edensahile28@gmail.com>
2025-12-05 20:14:46 +0100

    ✨ F01: LOGIN> Create LoginPage

---
+++ src/components/pages/LoginPage.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+
+export default function LoginPage() {
+ const [inputValue, setInputValue] = useState("")
+
+ const handleChange=(e)=>{
+ setInputValue(e.target.value);
+  
+
+ }
+
+ const handleSubmit=(e)=>{
+  e.preventDefault()
+   alert(`Bonjour ${inputValue}`)
+ setInputValue("");
+ }
+
+
+
+
+  return (
+   
+     <div>
+       <h1> Bienvenue chez nous ! </h1>
+       <h2>Connectez-vous</h2>
+       <form action="submit" onSubmit={handleSubmit}>
+        <input type="text" value={inputValue} placeholder="Entrez votre prénom..."onChange={handleChange}  required/>
+        <button>Accéder à votre espace</button>
+       </form>
+      
+      </div>
+  )
+}
