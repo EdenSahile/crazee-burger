d3e1f5bed4e456f69d9499023bf3ef444dd6e49f
Eden <edensahile28@gmail.com>
2025-12-06 14:59:04 +0100

    ✨ F02 : LOGIN> add redirection from/to OrderPage

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
 
 export default function LoginForm({inputValue,setInputValue}) {
 
@@ -21,7 +22,7 @@ export default function LoginForm({inputValue,setInputValue}) {
        <form action="submit" onSubmit={handleSubmit}>
         <input type="text" value={inputValue} placeholder="Entrez votre prénom..."onChange={handleChange}  required/>
         <button>Accéder à votre espace</button>
-      
+        < Link to="/order">Vers OrderPage</Link>
        </form>
       
       </div>
