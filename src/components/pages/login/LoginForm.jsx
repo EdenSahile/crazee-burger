b2560b358298ebe2fe43a2dbec0cc0d220c310e1
Eden <edensahile28@gmail.com>
2025-12-25 14:20:11 +0100

    💄 F03 : LoginForm> install styled components

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 export default function LoginForm({ inputValue, setInputValue }) {
   const handleChange = (e) => setInputValue(e.target.value);
@@ -14,10 +14,12 @@ export default function LoginForm({ inputValue, setInputValue }) {
   };
 
   return (
-    <div>
-      <h1> Bienvenue chez nous!</h1>
-      <h2>Connectez-vous</h2>
+     
       <form action="submit" onSubmit={handleSubmit}>
+         <h1 style={{color:"green"}}> Bienvenue chez nous!</h1>
+      <h2 style={{fontSize:15}}>Connectez-vous</h2>
+
+      <div className="cta-container">
         <input
           type="text"
           value={inputValue}
@@ -27,7 +29,9 @@ export default function LoginForm({ inputValue, setInputValue }) {
         />
 
         <button>Accéder à votre espace</button>
+        </div>
       </form>
-    </div>
+   
   );
 }
+
