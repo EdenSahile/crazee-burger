a0b8c5e6b6497e399e36b31b468911b30d07b6c0
Eden <edensahile28@gmail.com>
2026-01-01 21:57:59 +0100

    ✨ F04:ORDER> NAVBAR>greetings design

--- src/components/pages/order/Navbar.jsx
+++ src/components/pages/order/Navbar.jsx
@@ -1,64 +1,77 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import Logo from "../../reusable-ui/Logo"
+import Logo from "../../reusable-ui/Logo";
+// import { BsPersonCircle } from "react-icons/bs";
 
 export default function Navbar({ username }) {
-
   return (
     <NavbarStyled>
-      
       <div className="left-side">
-      <Logo />
-        </div>
-     
-      <div className="right-side">
-        <div className="right-side-name">
-        <h1> Bonjour {username}</h1>
-        <Link to="/">
-          <button> Déconnexion</button>
-        </Link>
-        </div>
-        <div className="right-side-icon">
-          ICON
-          </div>
-       
+        <Logo />
       </div>
-       
-      
+
+      <div className="right-side">
+        <div className="right-side-userInfo">
+          <h1 className="salutation">
+            {" "}
+            Hey, <span className="userName">{username}</span>
+          </h1>
+          <Link to="/">
+            <button>Se déconnecter</button>
+          </Link>
+        </div>
+        <div className="right-side-icon">ICON</div>
+      </div>
     </NavbarStyled>
   );
 }
 
 const NavbarStyled = styled.nav`
-    background-color: white;
-
- display: flex;
- justify-content: space-between;
-align-items: center;
-padding:0 70px 0 20px;
- height: 98.19px;
- border-radius: 15px 15px 0 0;
-
- .left-side{
-  transform: scale(0.4);
- 
-
-
-  
- }
-
-.right-side{
-    border:2px solid red;
+  background-color: white;
 
   display: flex;
+  justify-content: space-between;
   align-items: center;
-  gap:20px
+  padding: 0 70px 0 20px;
+  height: 98.19px;
+  border-radius: 15px 15px 0 0;
 
-
-
+  .left-side {
+    transform: scale(0.4);
   }
 
+  .right-side {
+    display: flex;
+    align-items: center;
+    gap: 20px;
+  }
+  .right-side-userInfo {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+  }
 
+  h1 {
+    font-size: 16px;
+    line-height: 22px;
+  }
 
-  
+  .salutation {
+    color: #747b91;
+
+    font-weight: 400;
+  }
+  .userName {
+    color: #ffa01b;
+    font-weight: 700;
+  }
+
+  button {
+    font-family: "Open Sans", sans-serif;
+    font-size: 10px;
+    line-height: 14px;
+    color: #747b91;
+    background-color: transparent;
+    border: none;
+  }
 `;
