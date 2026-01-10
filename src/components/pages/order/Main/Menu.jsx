9e005ee0d7da32ee779348f9b1fe9471f6468ec6
Eden <edensahile28@gmail.com>
2026-01-10 22:54:15 +0100

    🚧F05 : ORDER>MENU>Add product info

--- src/components/pages/order/Main/Menu.jsx
+++ src/components/pages/order/Main/Menu.jsx
@@ -1,52 +1,55 @@
-import React, { useState } from 'react'
-import styled from 'styled-components';
-import { fakeMenu2 } from "../../../../fakeData/fakeMenu"
-import { theme } from '../../../../theme';
+import React, { useState } from "react";
+import styled from "styled-components";
+import { fakeMenu2 } from "../../../../fakeData/fakeMenu";
+import { theme } from "../../../../theme";
 
 export default function Menu() {
+  const [menu, setmenu] = useState(fakeMenu2);
 
-const [menu, setmenu] = useState(fakeMenu2) 
+  console.log(menu);
+  return (
+    <MenuStyled className="menu">
+      {menu.map((produit) => {
+        return (
+          <div className="produit">
+            <div className="image">
+              <img src={produit.imageSource} alt={produit.title} />
+            </div>
 
-console.log(menu)
-return (
-    <MenuStyled className="menu"> 
-     
-    
-   {menu.map((produit)=>{
-   
-return <div className='produit'>{produit.title}</div>
-   })}
-   
-
-
-  
-
+            <div className="info-text">
+              <div className="title">{produit.title}</div>
+              <div className="description">
+                <div className="price">{produit.price}</div>
+                <button className="add-button">Ajouter</button>
+              </div>
+            </div>
+          </div>
+        );
+      })}
     </MenuStyled>
-  )
+  );
 }
 
-
 const MenuStyled = styled.div`
-
-  background-color:${theme.colors.background_white };
+  background-color: ${theme.colors.background_white};
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
-display: grid;
-grid-template-columns: repeat(4,1fr);
-grid-row-gap: 60px;
-justify-items: center;
-  box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-row-gap: 60px;
+  justify-items: center;
 
- 
+  .produit {
+    box-shadow: -8px 8px 20px 0px rgb(0 0 0 / 20%);
+    width: 240px;
+    height: 330px;
 
-
-
-  .produit{
-   
-     box-shadow: -8px 8px 20px 0px rgb(0 0 0 / 20%);
-    width:240px;
-    height:330px;
-
+    .image {
+      width: 100px;
+      height: auto;
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
   }
-  
-  
-`
+`;
