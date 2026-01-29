a67607d1cb185be93dacb3d2c6df63a8d1a6dd6c
Eden <edensahile28@gmail.com>
2026-01-29 20:50:54 +0100

    💄 F07: ADMIN>Add AdminTabs and AdmiinPanel

---
+++ src/components/pages/order/Main/Admin/AdminTabs.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function AdminTabs() {
+  return <AdminTabsSytled>AdminTabs</AdminTabsSytled>;
+}
+
+const AdminTabsSytled = styled.div`
+  border: 1px solid red;
+`;
