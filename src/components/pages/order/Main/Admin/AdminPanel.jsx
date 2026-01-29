f733a163d81c1da0cae744b9424f7415fe23e5c4
Eden <edensahile28@gmail.com>
2026-01-29 22:31:41 +0100

    💄 F07: ADMIN>Tabs>Add Tab

--- src/components/pages/order/Main/Admin/AdminPanel.jsx
+++ src/components/pages/order/Main/Admin/AdminPanel.jsx
@@ -3,15 +3,12 @@ import styled from "styled-components";
 import { theme } from "../../../../../theme";
 
 export default function AdminPanel() {
-  return <AdminPanelStyled>AdminPanel</AdminPanelStyled>;
+  return <AdminPanelStyled></AdminPanelStyled>;
 }
 
 const AdminPanelStyled = styled.div`
-  border: 1px solid;
   height: 250px;
-
   background-color: ${theme.colors.white};
   border: 1px solid ${theme.colors.greyLight};
   box-shadow: ${theme.shadows.subtle};
 `;
-s;
--- src/components/pages/order/Main/Admin/AdminTabs.jsx
+++ src/components/pages/order/Main/Admin/AdminTabs.jsx
@@ -1,10 +1,18 @@
-import React from "react";
 import styled from "styled-components";
+import Tab from "../../../../reusable-ui/Tab";
+import { FiChevronDown } from "react-icons/fi";
+import { AiOutlinePlus } from "react-icons/ai";
 
 export default function AdminTabs() {
-  return <AdminTabsSytled>AdminTabs</AdminTabsSytled>;
+  return (
+    <AdminTabsStyled>
+      <Tab icon={<FiChevronDown />} />
+      {/* <Tab icon={<AiOutlinePlus />} label="Ajouter un produit" /> */}
+    </AdminTabsStyled>
+  );
 }
 
-const AdminTabsSytled = styled.div`
-  border: 1px solid red;
+const AdminTabsStyled = styled.div`
+  display: flex;
+  padding: 0 20px;
 `;
