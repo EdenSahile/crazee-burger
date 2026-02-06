2a9a543ca7e300396b18ef3c81d96529b02c6a45
Eden <edensahile28@gmail.com>
2026-02-06 18:55:31 +0100

    🔧 F08 : Set up files to start ticke

--- src/components/pages/order/Main/Menu.jsx
+++ src/components/pages/order/Main/Menu.jsx
@@ -1,12 +1,12 @@
 import styled from "styled-components";
-import { fakeMenu2 } from "../../../../fakeData/fakeMenu";
+import { fakeMenu } from "../../../../fakeData/fakeMenu";
 import { theme } from "../../../../theme";
 import Card from "../../../reusable-ui/Card";
 import { formatPrice } from "../../../../utils/maths";
 import { useState } from "react";
 
 export default function Menu() {
-  const [menu, setmenu] = useState(fakeMenu2);
+  const [menu, setmenu] = useState(fakeMenu.MEDIUM);
 
   return (
     <MenuStyled className="menu">
