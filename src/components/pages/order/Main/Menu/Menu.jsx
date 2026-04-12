233422c651e1b832cf57314b0b59d884d3cef33f
Eden <edensahile28@gmail.com>
2026-04-12 11:09:15 +0200

    ✨ F09 : ADMIN>Always open EditPanel with the right product clicked on in the form

--- src/components/pages/order/Main/Menu/Menu.jsx
+++ src/components/pages/order/Main/Menu/Menu.jsx
@@ -19,6 +19,8 @@ export default function Menu() {
     resetMenu,
     setProductSelected,
     productSelected,
+    setIsCollapsed,
+    setCurrentTabSelected,
   } = useContext(OrderContext);
 
   if (menu.length === 0) {
@@ -28,6 +30,9 @@ export default function Menu() {
 
   const handleClick = (idProductClicked) => {
     if (!isModeAdmin) return;
+
+    setIsCollapsed(false);
+    setCurrentTabSelected("edit");
     const productClickedOn = menu.find(
       (product) => product.id === idProductClicked,
     );
