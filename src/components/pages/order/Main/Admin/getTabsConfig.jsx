e42ce0e8e8875468b85528ec647103abdf3e47b7
Eden <edensahile28@gmail.com>
2026-05-14 21:09:17 +0200

    🔥 F09 : ADMINPANEL > remove example

--- src/components/pages/order/Main/Admin/getTabsConfig.jsx
+++ src/components/pages/order/Main/Admin/getTabsConfig.jsx
@@ -3,10 +3,6 @@ import { MdModeEditOutline } from "react-icons/md";
 import AddForm from "./AdminPanel/AddForm";
 import EditForm from "./AdminPanel/EditForm";
 import HintMessage from "./AdminPanel/HintMessage";
-import { FiSettings } from "react-icons/fi";
-import AdminForm from "./AdminPanel/AdminForm";
-import { EMPTY_PRODUCT } from "../../../../../enums/product";
-import Button from "../../../../reusable-ui/Button";
 
 export const getTabsConfig = (hasAlreadyBeenClicked) => [
   {
@@ -19,27 +15,10 @@ export const getTabsConfig = (hasAlreadyBeenClicked) => [
     index: "edit",
     icon: <MdModeEditOutline />,
     content: hasAlreadyBeenClicked ? <EditForm /> : <HintMessage />,
-
     label: "Modifier un produit",
-  },
-  {
-    index: "manger",
-    icon: <FiSettings />,
-    content: <UnComposant />,
-    label: "Autre onglet",
   },
 ];
 
 export const getTabSelected = (tabs, currentTabSelected) => {
   return tabs.find((tab) => tab.index === currentTabSelected);
 };
-
-export function UnComposant() {
-  return (
-    <AdminForm product={EMPTY_PRODUCT}>
-      <div style={{ width: "20%" }}>
-        <Button version="normal" label="bonjour" />
-      </div>
-    </AdminForm>
-  );
-}
